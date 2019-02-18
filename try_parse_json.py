fbf1b09d996824c1e517c3a02cff3eec1a841bdb
dkurchigin from work <kurchigin.dmitry@yandex.ru>
2019-02-18 18:14:29 +0300

    try check lists

--- try_parse_json.py
+++ try_parse_json.py
@@ -21,6 +21,30 @@ json_string = """{
 } """
  
 # распарсенная строка
-parsed_string = json.loads(json_string)
-print (len(parsed_string["orderContents"]))
-print (parsed_string["orderContents"][1])
+#parsed_string = json.loads(json_string)
+#f = open("RobotizationCalls.json", 'r', encoding='utf-8')
+#for line in f:
+    #print (line)
+
+def recursion_print_elements(first_element, section):
+    print (first_element)
+    for element in first_element:
+        print (element + ":")
+        print (parsed_string[section][element])
+        print ("\n")
+        if parsed_string[section][element] in element:
+            print ("true")
+        
+        #recursion_print_elements(parsed_string[element],element)
+    #if "RobotizationCalls" in "script_types":
+        #print ("true")
+    #else:
+        #print ("false")
+        
+with open("RobotizationCalls.json", "r", encoding='utf-8') as read_file:
+    parsed_string = json.load(read_file)
+
+#print (globals_params)
+section = "globals"
+globals_params = (parsed_string[section])
+recursion_print_elements(globals_params, section)
